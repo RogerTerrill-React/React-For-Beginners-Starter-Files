f571429b3ce471d8807ebbaec87937771e0616cc
Roger Terrill <rchicasterill@gmail.com>
2019-02-19 16:06:45 -0800

    Persisting Order State with localstorage

--- catch-of-the-day/src/components/Order.js
+++ catch-of-the-day/src/components/Order.js
@@ -5,9 +5,15 @@ class Order extends Component {
   renderOrder = key => {
     const fish = this.props.fishes[key];
     const count = this.props.order[key];
-    const isAvailable = fish.status === 'available';
+    const isAvailable = fish && fish.status === 'available';
+    // Make sure the fish is loaded before we cont
+    if (!fish) return null;
     if (!isAvailable) {
-      return <li key={key}>Sorry {fish ? fish.name : 'fish'} is no longer available</li>;
+      return (
+        <li key={key}>
+          Sorry {fish ? fish.name : 'fish'} is no longer available
+        </li>
+      );
     }
     return (
       <li key={key}>
