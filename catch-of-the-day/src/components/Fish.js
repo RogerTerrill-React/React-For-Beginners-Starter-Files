167478166965a4cc2f7d4532651c137911f2d5d0
Roger Terrill <rchicasterill@gmail.com>
2019-02-22 09:56:23 -0800

    not displaying

--- catch-of-the-day/src/components/Fish.js
+++ catch-of-the-day/src/components/Fish.js
@@ -1,9 +1,18 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import { formatPrice } from '../helpers';
+
 class Fish extends Component {
-  handleClick = () => {
-    this.props.addToOrder(this.props.index);
-  };
+  static propTypes = {
+    details: PropTypes.shape({
+      image: PropTypes.string,
+      name: PropTypes.string,
+      desc: PropTypes.string,
+      status: PropTypes.string,
+      price: PropTypes.number
+    }),
+    addToOrder: PropTypes.func,
+  }
 
   render() {
     const { image, name, price, desc, status } = this.props.details;
@@ -17,7 +26,7 @@ class Fish extends Component {
           <span className="price">{formatPrice(price)}</span>
         </h3>
         <p>{desc}</p>
-        <button disabled={!isAvailable} onClick={this.handleClick}>
+        <button disabled={!isAvailable} onClick={this.props.addToOrder(this.props.index)}>
           {isAvailable ? 'Add To Cart' : 'Sold Out'}
         </button>
       </li>
